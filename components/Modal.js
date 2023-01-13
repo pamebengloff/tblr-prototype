a9e714c2470ace36b3d3e9e31df64f4846d61874
Pamela Benavides <pamebengloff@gmail.com>
2023-01-12 17:47:57 -0700

    modal fix

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import modalStyles from '../styles/Modal.module.css';
 import UserIcon from "./UserIcon"
-
+import {AiFillTool} from "react-icons/ai"
+//import {BiChevronDown} from "react-icons/bi"
 
 // isOpen: a boolean flag that represents the modal’s state (open or closed) and is controlled in the parent component that renders the modal
 //handleClose: a method that is called by clicking the close button or by any action that triggers a close
@@ -25,17 +26,22 @@ return(
 <div className={modalStyles.container}>
    
     <div className={modalStyles.semiContainer}> 
-
+    <div className={modalStyles.semisemiContainer}> 
     {/* para acomodar al user icon */}
     <div className={modalStyles.userIcon}>
         <UserIcon  />
     </div>
     {/* para acomodar el cuadro para postear*/}
     <div className={modalStyles.modalBox}>        
-        
+        <div className={modalStyles.modalTop}>
+            <button> pamebengloff </button>  {/*aqui debe ir el username de quien se loguee */}
+            <button> <AiFillTool/> </button>
+        </div>
         {/* todo el contenido sobre Text, Photo, Quote, etc: */}
         {children} 
         {/* para acomodar los botones de close y publish */}
+        
+       
         <div className={modalStyles.modalFooter}>
             <button onClick={handleClose} >
                 Close
@@ -47,6 +53,7 @@ return(
      
     </div>   
 
+    </div>   
     </div>  
 </div>
 </>)}
--- components/Text.js
+++ components/Text.js
@@ -3,18 +3,28 @@ import Modal from "./Modal";
 
 export default function TextPage(){
 
-   
+    const [hasContent, setHasContent] = useState(false);
+
 return(<>
      
-         soy textooo
-         <p>
+     <h1>Titulo</h1>
+    
+    <p>  Aquí va el texto  </p>
+         <br/>
+         {/* <p>
+
+
      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a lacinia neque. Donec nec posuere enim. Donec nulla erat, accumsan ut arcu et, laoreet mattis arcu. Cras sit amet maximus neque. Fusce viverra, lorem sit amet egestas tincidunt, tortor magna posuere tortor, egestas pulvinar erat magna non tellus. Nunc accumsan, ante ac cursus condimentum, odio dui sollicitudin libero, at posuere tortor ante et metus. Phasellus at condimentum felis. Donec eu auctor risus. Praesent vitae gravida augue. Maecenas tincidunt hendrerit rhoncus. Duis id sapien vel mi ornare fringilla. Curabitur mattis nisl vel nibh iaculis, vehicula dignissim lectus varius. Donec sed ex dictum, semper augue a, tincidunt risus.
 Nulla facilisi. Mauris feugiat velit nec purus blandit aliquet. Nunc molestie dictum dui ac vestibulum. Duis quis tortor vel nunc ornare malesuada. Phasellus eget justo justo. Morbi varius sapien est, nec rutrum enim commodo id. Ut vel nunc eget lectus consectetur fermentum et in elit. Integer odio lacus, fermentum eu orci vitae, feugiat rutrum augue. Cras eu lorem quis dui varius posuere.
 Mauris sit amet mollis libero. Sed sed porttitor orci, non facilisis dolor. Aenean molestie iaculis sapien at auctor. Etiam placerat augue sit amet tellus sagittis convallis et nec dui. Sed in interdum massa. Duis semper est nulla, id hendrerit felis placerat sed. Phasellus eget cursus sem. Nunc vel sollicitudin quam. Integer urna tortor, sagittis sed lacus sit amet, blandit scelerisque sapien. Donec id cursus est. Nam molestie quam nec rhoncus euismod. Vestibulum molestie cursus magna vitae suscipit.
 Fusce quam est, pharetra bibendum semper a, egestas imperdiet tortor. Duis posuere, est et mattis posuere, nibh odio porttitor diam, vel dignissim erat augue sed metus. Quisque convallis nunc lobortis sem volutpat, sit amet ornare augue vehicula. Quisque molestie tincidunt fringilla. In id libero a tellus bibendum lacinia vitae quis ex. Nullam hendrerit, risus et laoreet pellentesque, metus mauris malesuada felis, ut pretium tortor leo eget dui. Phasellus quis fringilla erat. Etiam nisi mauris, ultrices et dapibus non, condimentum at nibh. Integer at pharetra lacus, varius blandit dui. Maecenas aliquam tempus lorem, vitae efficitur nibh vehicula ac. Sed sodales quis nisl quis pellentesque. Donec id purus ornare, semper orci ac, suscipit metus.
 Curabitur et justo nec felis interdum luctus mollis sed quam. Fusce at gravida erat. Donec non tristique justo, ullamcorper luctus sapien. Proin metus eros, ullamcorper vel aliquam et, consequat ac mi. Pellentesque gravida arcu id lacus congue luctus. Vivamus nec tellus iaculis, auctor tortor eu, rhoncus urna. Maecenas nec vehicula nisl, eget varius purus. In tristique tortor ut nunc gravida, vel auctor urna viverra.
-     </p>
+     </p> */}
   
+  { hasContent ?
+        <h5> #tags </h5>
+            : null
+        }
 
     </>)
 }
